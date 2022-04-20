33b11612a502317652c8bea051ea6510724e5692
schillerandrew <schiller.andrew@gmail.com>
2022-04-19 21:00:49 -0700

    corrected rendering of city map, built out code structure across multiple components

---
+++ src/Main.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import CityRender from './CityRender.js';
+import './Main.css';
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <main>
+        <Form onSubmit={this.props.handleExploreClick} onInput={this.props.handleCityInput}>
+          <Form.Control type="text" placeholder="Enter in a city" />
+          <Button type="submit">Explore!</Button>
+        </Form>
+        <CityRender
+          cityData={this.props.cityData}
+        />
+      </main>
+    )
+  }
+}
+
+export default Main;
