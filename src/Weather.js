9bfa4761b35b475154c83be001e6bf77691e5404
schillerandrew <schiller.andrew@gmail.com>
2022-04-21 15:51:24 -0700

    corrected handling and rendering of forecast data from server

--- src/Weather.js
+++ src/Weather.js
@@ -6,11 +6,11 @@ class Weather extends React.Component {
   render() {
     return (
       <>
-        <Card.Text>The forecast for {this.props.weatherData[0].date} is {this.props.weatherData[0].desc.toLowerCase()}.
+        <Card.Text>The forecast for {this.props.weatherData[0].date} is a high of {Math.round(this.props.weatherData[0].highTemp)} and a low of {Math.round(this.props.weatherData[0].lowTemp)}, with {this.props.weatherData[0].clouds}% cloud coverage.
         </Card.Text>
-        <Card.Text>The forecast for {this.props.weatherData[1].date} is {this.props.weatherData[1].desc.toLowerCase()}.
+        <Card.Text>The forecast for {this.props.weatherData[1].date} is a high of {Math.round(this.props.weatherData[1].highTemp)} and a low of {Math.round(this.props.weatherData[1].lowTemp)}, with {this.props.weatherData[1].clouds}% cloud coverage.
         </Card.Text>
-        <Card.Text>The forecast for {this.props.weatherData[2].date} is {this.props.weatherData[2].desc.toLowerCase()}.
+        <Card.Text>The forecast for {this.props.weatherData[2].date} is a high of {Math.round(this.props.weatherData[2].highTemp)} and a low of {Math.round(this.props.weatherData[2].lowTemp)}, with {this.props.weatherData[2].clouds}% cloud coverage.
         </Card.Text>
       </>
     )
